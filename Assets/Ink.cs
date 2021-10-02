68a978e6279099fd253c16f05c822eb19eb74c33
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-10-02 14:03:15 -0700

    Add basic camera scipt

---
+++ Assets/Ink.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Ink : MonoBehaviour {
+
+    public void onRenderImage(RenderTexture target, RenderTexture destination) {
+        Graphics.Blit(target, destination);
+    }
+}
